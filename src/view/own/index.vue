15aefacbc705319958dbdb52b99eebd49c9baacf
Youcong Lu <1516116319@qq.com>
2020-07-27 18:02:11 +0800

    OwnV2-Static

--- src/view/own/index.vue
+++ src/view/own/index.vue
@@ -3,7 +3,7 @@
         
             <el-col v-for="(item,index) in   productList" :key="index" :span="6" :offset="1">
         
-                <el-card class="realEstate-card">
+                <el-card class="productOwn-card">
                     <div slot="header" class="clearfix">
                     持有状态:
                     <span style="color: rgb(255, 0, 0);">{{item.status}}</span>
@@ -12,6 +12,10 @@
                 <div class="item">
                     <el-tag>产品ID: </el-tag>
                     <span>{{item.productId}}</span>
+                </div>
+                <div class="item">
+                    <el-tag>策略名称: </el-tag>
+                    <span>{{item.productName}}</span>
                 </div>
                 <div class="item">
                     <el-tag type="success">发行方ID: </el-tag>
@@ -26,10 +30,33 @@
                     <span>{{item.setBack}}</span>
                 </div>
 
+                <div>
+                    <el-button type="text" @click="MarketSignalTableVisible = true">市场信号</el-button>
+                    <el-divider direction="vertical" />
+                    <el-button type="text" @click="OpsSignalTableVisible = true">操作信号</el-button>
+                </div>
                 
                 </el-card>
         
             </el-col>
+
+            <el-dialog title="市场信号" :visible.sync="MarketSignalTableVisible">
+              <el-table :data="marketSignalData">
+                <el-table-column property="id" label="ID" width="150"></el-table-column>
+                <el-table-column property="date" label="日期" width="150"></el-table-column>
+                <el-table-column property="stock" label="股票" width="200"></el-table-column>
+                <el-table-column property="price" label="价格"></el-table-column>
+              </el-table>
+            </el-dialog>
+
+            <el-dialog title="操作信号" :visible.sync="OpsSignalTableVisible">
+              <el-table :data="OpsSignalData">
+                <el-table-column property="id" label="ID" width="150"></el-table-column>
+                <el-table-column property="date" label="日期" width="150"></el-table-column>
+                <el-table-column property="stock" label="股票" width="200"></el-table-column>
+                <el-table-column property="ops" label="操作"></el-table-column>
+              </el-table>
+            </el-dialog>
          
     </div>
 </template>
@@ -39,29 +66,64 @@ export default {
   data(){
 
       return{
+        MarketSignalTableVisible : false,
+        OpsSignalTableVisible : false,
           loading: true,
           productList:[
               
               
                   {  
                       status:'True',
-                      productId:'0',
-                      producerId:'001',
-                      profit:'10%',
-                      setBack:'20%'
+                      productId:'200010002',
+                      productName: '网格策略',
+                      producerId:'10110001',
+                      profit:'5%',
+                      setBack:'17%'
 
                   },
                   {
                       status:'True',
-                      productId:'0',
-                      producerId:'001',
+                      productId:'20001001',
+                      productName: '海龟交易策略',
+                      producerId:'10110002',
                       profit:'10%',
                       setBack:'20%'
 
-                  }
+                  },
+                  
               
 
           ],
+
+          marketSignalData:[
+            {
+              id : '00000001',
+              date:'2020-05-01',
+              stock :'Stock_A',
+              price : 10,
+            },
+            {
+              id : '00000002',
+              date:'2020-05-02',
+              stock :'Stock_A',
+              price : 11,
+            },
+          ],
+
+          OpsSignalData:[
+            {
+              id : '00010001',
+              date:'2020-05-01',
+              stock :'Stock_A',
+              ops : '+1',
+            },
+            {
+              id : '00010002',
+              date:'2020-05-02',
+              stock :'Stock_A',
+              ops : '-1',
+            },
+          ]
       }
   }
 }
@@ -90,9 +152,9 @@ export default {
   .clearfix:after {
     clear: both
   }
-.realEstate-card {
-    width: 250px;
-    height: 340px;
+.productOwn-card {
+    width: 280px;
+    height: 380px;
     margin: 18px;
   }
   
