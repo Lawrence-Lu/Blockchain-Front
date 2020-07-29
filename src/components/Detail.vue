cddb5730ff6f074a5f7905978909d9d65271e915
HideOnRose <49604075+HideOnRose@users.noreply.github.com>
2020-07-29 17:43:25 +0800

    Add files via upload

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -17,13 +17,28 @@
               <el-form-item label="邮箱">
                 <el-col :span="8"> <el-input size="mini"></el-input></el-col>
               </el-form-item>
-              <el-form-item label="风险承受能力">
-                <el-col :span="8">
-                  <el-input size="mini"></el-input>
-                </el-col>
-              </el-form-item>
             </el-form>
             <div class="grid-content bg-purple"></div>
+          </el-tab-pane>
+
+          <el-tab-pane label="风险承受能力" name="second">
+            <el-form>
+              <el-form-item label="主要收入来源">
+                <el-col :span="8"> <el-input size="mini"></el-input></el-col>
+              </el-form-item>
+              <el-form-item label="最近三年个人年均收入">
+                <el-col :span="8"><el-input size="mini"></el-input></el-col>
+              </el-form-item>
+              <el-form-item label="投资资金占家庭总资产比例">
+                <el-col :span="8"> <el-input size="mini"></el-input></el-col>
+              </el-form-item>
+              <el-form-item label="可用于投资的资产数额">
+                <el-col :span="8"> <el-input size="mini"></el-input></el-col>
+              </el-form-item>
+              <el-form-item label="可承受最大投资损失">
+                <el-col :span="8"> <el-input size="mini"></el-input></el-col>
+              </el-form-item>
+            </el-form>
           </el-tab-pane>
         </el-tabs>
       </div>
@@ -35,9 +50,7 @@
 export default {
   name: "Detail",
   data() {
-    return {
-      id: "123"
-    };
+    return {};
   },
   components: {},
   mounted() {},
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 
-import total from "../components/total.vue";
+import Detail from "../components/Detail.vue";
 
 Vue.use(Router);
 
@@ -10,7 +10,7 @@ export default new Router({
     {
       path: "/",
       name: "info",
-      component: total
+      component: Detail
     }
   ]
 });
